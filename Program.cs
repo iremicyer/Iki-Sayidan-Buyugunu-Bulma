f408d4631662ba288e91e7045b0d3cb2e97105ba
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-08-13 21:50:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IkiSayidanBüyüğünüBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Birinci sayiyi giriniz:");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ikinci sayiyi giriniz:");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi1 > sayi2)
+            {
+                Console.WriteLine("Birinci sayi büyüktür");
+            }
+            else if(sayi2 == sayi1)
+            {
+                Console.WriteLine("Iki sayi esittir");
+            }
+            else
+            {
+                Console.WriteLine("Ikinci sayi büyüktür");
+            }
+        }
+    }
+}
